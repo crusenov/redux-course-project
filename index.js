5be9ea678bffbff4a1d0d4cf084f661fbd8941e3
legion <tsvetan@airheadventures.com>
2019-12-23 10:52:43 +0200

    Combine reducers

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-
-
-{
-  type: 'ADD_TODO',
-  todo: {
-    id: 0,
-    name: 'Learn Redux',
-    complete: false
-  }
-}
-
-{
-  type: 'REMOVE_TODO',
-  id: 0
-}
-
-{
-  type: 'TOGGLE_TODO',
-  id: 0
-}
-
-{
-  type: 'ADD_GOAL',
-  goal: {
-    id: 0,
-    name: 'Run a Marathon'
-  }
-}
-
-{
-  type: 'REMOVE_GOAL',
-  id: 0
-}
-
 /*
   Characteristics of a Pure Function
   1) They always return the same result if the same arguments are passed in.
@@ -55,6 +21,24 @@ function todos (state = [], action) {
 
       default: 
         return state
+  }
+}
+
+function goals (state = [], action) {
+  switch (action.type) {
+    case 'ADD_GOAL':
+      return state.concat([action.goal]) 
+    case 'REMOVE_GOAL':
+      return state.filter((goal) => goal.id !== action.id)
+    default:
+      return state
+  }
+}
+
+function app (state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
   }
 }
 
@@ -92,4 +76,66 @@ function createStore (reducer) {
   }
 }
 
-const store = createStore(todos)
+const store = createStore(app)
+
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Walk the dog',
+    complete: false,
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 1,
+    name: 'Wash the car',
+    complete: false,
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 2,
+    name: 'Go to the gym',
+    complete: true,
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_TODO',
+  id: 1
+})
+
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Learn Redux'
+  }
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  goal: {
+    id: 1,
+    name: 'Lose 20 pounds'
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_GOAL',
+  id: 0
+})
