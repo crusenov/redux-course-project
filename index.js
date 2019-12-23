0c5d23d524e619a3942783deb2c44622e85c7672
legion <tsvetan@airheadventures.com>
2019-12-23 11:01:30 +0200

    Leveraging Constants

--- index.js
+++ index.js
@@ -5,43 +5,8 @@
   3) Never produce any side effects.
 */
 
-// Reducer function
-function todos (state = [], action) {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return state.concat([action.todo])
 
-    case 'REMOVE_TODO':
-      return state.filter((todo) => todo.id !== action.id)
-
-    case 'TOGGLE_TODO':
-      return state.map((todo) => todo.id !== action.id ? todo : 
-        Object.assign({}, todo, {complete: !todo.complete})  
-      )
-
-      default: 
-        return state
-  }
-}
-
-function goals (state = [], action) {
-  switch (action.type) {
-    case 'ADD_GOAL':
-      return state.concat([action.goal]) 
-    case 'REMOVE_GOAL':
-      return state.filter((goal) => goal.id !== action.id)
-    default:
-      return state
-  }
-}
-
-function app (state = {}, action) {
-  return {
-    todos: todos(state.todos, action),
-    goals: goals(state.goals, action)
-  }
-}
-
+// Library code
 function createStore (reducer) {
   // The store should have four parts
   // 1. The state
@@ -76,6 +41,51 @@ function createStore (reducer) {
   }
 }
 
+// App code
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+
+// Reducer function
+function todos (state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo])
+
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id)
+
+    case TOGGLE_TODO:
+      return state.map((todo) => todo.id !== action.id ? todo : 
+        Object.assign({}, todo, {complete: !todo.complete})  
+      )
+
+      default: 
+        return state
+  }
+}
+
+function goals (state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal]) 
+    case REMOVE_GOAL:
+      return state.filter((goal) => goal.id !== action.id)
+    default:
+      return state
+  }
+}
+
+function app (state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  }
+}
+
 const store = createStore(app)
 
 store.subscribe(() => {
@@ -83,7 +93,7 @@ store.subscribe(() => {
 })
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 0,
     name: 'Walk the dog',
@@ -92,7 +102,7 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 1,
     name: 'Wash the car',
@@ -101,7 +111,7 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 2,
     name: 'Go to the gym',
@@ -110,17 +120,17 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'REMOVE_TODO',
+  type: REMOVE_TODO,
   id: 1
 })
 
 store.dispatch({
-  type: 'TOGGLE_TODO',
+  type: TOGGLE_TODO,
   id: 0
 })
 
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: {
     id: 0,
     name: 'Learn Redux'
@@ -128,7 +138,7 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: {
     id: 1,
     name: 'Lose 20 pounds'
@@ -136,6 +146,6 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'REMOVE_GOAL',
+  type: REMOVE_GOAL,
   id: 0
 })
